5c536df544a24552e6f78646d9846a989c2c1da1
Josh Long <josh@joshlong.com>
2021-10-25 23:16:03 -0700

    up

--- demo/README.md
+++ demo/README.md
@@ -1,7 +1,44 @@
-# Bootiful GraphQL 
+# Bootiful GraphQL
 
-# GraphQL
+## Outline
+
+* the tension at FB
+* the Spring Boot starter
+* building a `RuntimeWiringConfigurer` to handle a `query` called `hello` that
+  takes an argument called `name`
+* building a `@Controller` to handle a `@SchemaMapping` that returns the same
+  thing but with a different language so we can see its working. Be sure to
+  comment out the `RuntimeWiringConfigurer`.
+* Then change the `@SchemaMapping` to be a `@QueryMapping`, as that's more concise.
+* Then introduce the concept of a `Customer` whose data we can read from
+  a `Map <Integer, Customer>`. Create the `Customer` with a `record`
+* Create a `query` for `customers`.
+* We need to add data, though, don't we? add a constructor that bootstraps 8 records. the logic we use to add a customer tho? We can reuse that for a client! 
+* Create a mutation to `addCustomer(name:String ): Customer`
+* Great, but let's suppose we wanted to add a field to explain when someone
+  enrolled. We need a `Date`, but GraphQL doesn't support a `Date` out of the
+  box. You can easily handle this with a customer `resolver` (controller handler
+  method) that uses a `SimpleDataFormat` in a threadlocal to return
+  a `String` (see below for the code).
+* this ability, to mesh resolvers to form the whole object graph independent of the underlying representation, is one of the reasons graphql is so powerful. let's suppose we wanted to provide `Order` data for each customer, as well. 
+* 
+
+## GraphQL
+
+the following incantation is handy to construct a `SimpleDateFormat`:
+
+```
+new SimpleDateFormat("yyyy-MM-dd'T'HH:mm'Z'"){{
+	setTimeZone(TimeZone.getTimeZone("UTC"));
+}};
+```
+
+the following incantation can be used to invoke an endpoint with security
+applied
+
+```
 curl  -v -u jlong:pw http://localhost:8080/graphql \
 -H 'Content-Type: application/json' \
 --data-raw '{"query":"query { secureHello }" }'
+```
  
--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,14 +1,14 @@
 package com.example.demo;
 
-import graphql.schema.DataFetcher;
-import graphql.schema.DataFetchingEnvironment;
 import graphql.schema.idl.RuntimeWiring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.graphql.data.method.annotation.SchemaMapping;
 import org.springframework.graphql.execution.RuntimeWiringConfigurer;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
@@ -18,9 +18,13 @@ import org.springframework.security.core.userdetails.MapReactiveUserDetailsServi
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.stereotype.Controller;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import java.security.Principal;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -71,8 +75,45 @@ class SimpleRuntimeWiringConfigurer implements RuntimeWiringConfigurer {
 	}
 }
 
+
+record Customer(Integer id, Date enrollment, String name) {
+}
+
 @Controller
 class HelloGraphqlController {
+
+
+	private final Map<Integer, Customer> db = new ConcurrentHashMap<>();
+	private final AtomicInteger id = new AtomicInteger();
+
+	HelloGraphqlController() {
+		List.of(
+				"Yuxin", "Violetta", "Olga", "Madhura",
+				"Josh", "Dave", "Yuxin", "Spencer")
+			.forEach(this::buildCustomer);
+	}
+
+	@MutationMapping
+	Mono<Customer> addCustomer(@Argument String name) {
+		var key = buildCustomer(name);
+		return Mono.just(this.db.get(key));
+	}
+
+	private int buildCustomer(String name) {
+		var key = this.id.incrementAndGet();
+		this.db.put(key, new Customer(key, new Date(), name));
+		return key;
+	}
+
+	@SchemaMapping(typeName = "Customer")
+	String date(Customer customer) {
+		return buildIsoDateFormat().format(customer.enrollment());
+	}
+
+	@QueryMapping
+	Flux<Customer> customers() {
+		return Flux.fromIterable(this.db.values());
+	}
 
 	@QueryMapping
 	Mono<String> secureHello() {
@@ -82,4 +123,34 @@ class HelloGraphqlController {
 			.map(auth -> "Hello, " + auth.getName() + "!");
 	}
 
+
+	private final ThreadLocal<SimpleDateFormat> df = new ThreadLocal<>();
+
+	private SimpleDateFormat buildIsoDateFormat() {
+		if (this.df.get() == null) {
+			var sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm'Z'") {
+				{
+					setTimeZone(TimeZone.getTimeZone("UTC"));
+				}
+			};
+
+			this.df.set(sdf);
+		}
+
+		return this.df.get();
+	}
+
+	@SchemaMapping(typeName = "Customer")
+	Flux<Order> orders(Customer customer) {
+		var list = new ArrayList<Order>();
+		var max = Math.random() * 100;
+		for (var id = 1; id < max; id++) list.add(new Order(id, customer.id()));
+		return Flux.fromIterable(list);
+	}
+
 }
+
+
+record Order(Integer id, Integer customerId) {
+}
+
